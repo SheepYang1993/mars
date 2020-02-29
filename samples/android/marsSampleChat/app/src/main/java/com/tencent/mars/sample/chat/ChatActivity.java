415ddaa438354237420050f46966e430357147fe
SheepYang <332594623@qq..com>
2020-02-29 10:08:34 +0800

    格式化。。。

--- samples/android/marsSampleChat/app/src/main/java/com/tencent/mars/sample/chat/ChatActivity.java
+++ samples/android/marsSampleChat/app/src/main/java/com/tencent/mars/sample/chat/ChatActivity.java
@@ -1,16 +1,16 @@
 /*
-* Tencent is pleased to support the open source community by making Mars available.
-* Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
-*
-* Licensed under the MIT License (the "License"); you may not use this file except in 
-* compliance with the License. You may obtain a copy of the License at
-* http://opensource.org/licenses/MIT
-*
-* Unless required by applicable law or agreed to in writing, software distributed under the License is
-* distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-* either express or implied. See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Tencent is pleased to support the open source community by making Mars available.
+ * Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.tencent.mars.sample.chat;
 
@@ -67,6 +67,7 @@ public class ChatActivity extends AppCompatActivity implements Observer {
 
     private ChatMsgViewAdapter adapter;// 消息视图的Adapter
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
@@ -158,6 +159,7 @@ public class ChatActivity extends AppCompatActivity implements Observer {
         MarsServiceProxy.inst.setForeground(true);
     }
 
+    @Override
     public void onPause() {
         super.onPause();
         MarsServiceProxy.inst.setForeground(false);
